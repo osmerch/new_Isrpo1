d710bdd4dc2e06861ffb0b439eda8396e93d07ba
Sonya <ellie_greys@mail.ru>
2023-04-24 14:10:10 +0300

    Add event handler

--- new_Isrpo1/NewWin.xaml.cs
+++ new_Isrpo1/NewWin.xaml.cs
@@ -23,5 +23,10 @@ namespace new_Isrpo1
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
